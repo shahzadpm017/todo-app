f66776ac0f526d744522cbcadd3b4256ee192afc
SHAHZAD <shahzadpm016@gmail.com>
2025-03-19 04:18:52 +0530

    initial commit

--- todo/functions.py
+++ todo/functions.py
@@ -7,3 +7,9 @@ def get_todos(filepath="todos.txt"):
 def write_todos(todos_arg,filepath="todos.txt"):
     with open(filepath,'w') as file_local:
         file_local.writelines(todos_arg)
+
+if __name__=="__main__":
+    print("Hello")
+    print(get_todos())
+
+
