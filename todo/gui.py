aca9ffe6e236c0433320adc6d0ac7fb6b9635509
SHAHZAD <shahzadpm016@gmail.com>
2025-04-05 19:51:53 +0530

    added complete and add button images

--- todo/gui.py
+++ todo/gui.py
@@ -3,14 +3,14 @@ import FreeSimpleGUI as sg
 import time
 
 sg.theme("black")
-clock=sg.Text("",key="clock")
+clock=sg.Text('',key="clock")
 label=sg.Text("Type in a to-do")
 input_box=sg.InputText(tooltip="enter to-do",key="todo")
-add_button=sg.Button("Add")
+add_button=sg.Button(key="Add",image_source="add.png",size=2,mouseover_colors="LightBlue2",tooltip="add button")
 edit_button=sg.Button("Edit")
 list_box=sg.Listbox(values=functions.get_todos(),key='todo_listbox',enable_events=True,
                     size=[45,10])
-complete_button=sg.Button("Complete")
+complete_button=sg.Button(key="Complete",image_source="complete.png",size=2,mouseover_colors="LightBlue2",tooltip="complete button")
 exit_button=sg.Button("Exit")
 
 
@@ -26,7 +26,7 @@ while True:
             new_todo=values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
-            window['todo_edit'].update(values=todos)
+            window['todo_listbox'].update(values=todos)
         case "Edit":
             try:
                 todo_to_edit = values['todo_listbox'][0]
