6874e2e303fb7f191e3ca169346980b320ff7b2b
SHAHZAD <shahzadpm016@gmail.com>
2025-04-05 18:57:46 +0530

    implement complete and exit button

--- todo/gui.py
+++ todo/gui.py
@@ -5,12 +5,14 @@ label=sg.Text("Type in a to-do")
 input_box=sg.InputText(tooltip="enter to-do",key="todo")
 add_button=sg.Button("Add")
 edit_button=sg.Button("Edit")
-list_box=sg.Listbox(values=functions.get_todos(),key='todo_edit',enable_events=True,
+list_box=sg.Listbox(values=functions.get_todos(),key='todo_listbox',enable_events=True,
                     size=[45,10])
+complete_button=sg.Button("Complete")
+exit_button=sg.Button("Exit")
 
 
 window= sg.Window('My To-do App',
-                  layout=[[label] , [input_box,add_button],[list_box,edit_button]],
+                  layout=[[label] , [input_box,add_button],[list_box,edit_button,complete_button],[exit_button]],
                   font=('Helvetica',12))
 while True:
     event,values=window.read()
@@ -24,7 +26,7 @@ while True:
             functions.write_todos(todos)
             window['todo_edit'].update(values=todos)
         case "Edit":
-            todo_to_edit = values['todo_edit'][0]
+            todo_to_edit = values['todo_listbox'][0]
             new_todo=values['todo']+"\n"
 
             todos=functions.get_todos()
@@ -32,9 +34,18 @@ while True:
             todos[index]=new_todo
             functions.write_todos(todos)
 
-            window['todo_edit'].update(values=todos)
-        case 'todo_edit':
-            window['todo'].update(value=values['todo_edit'][0])
+            window['todo_listbox'].update(values=todos)
+        case 'todo_listbox':
+            window['todo'].update(value=values['todo_listbox'][0])
+        case "Complete":
+            todo_to_complete=values["todo_listbox"][0]
+            todos=functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todo_listbox"].update(values=todos)
+            window["todo"].update(value='')
+        case "Exit":
+            break
 
 
         case sg.WIN_CLOSED:
